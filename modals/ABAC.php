af73957751d38e5f338d35f78ff83dc6edcb045e
DarioTortorici <95847496+DarioTortorici@users.noreply.github.com>
2023-07-14 10:44:42 +0200

    Rebuilt recording system
    Changed the recording system using ffmpeg

--- modals/ABAC.php
+++ modals/ABAC.php
@@ -1,7 +1,7 @@
 <?php
 
 require(__DIR__ . '/../authentication/db_connection.php');
-
+include(__DIR__ . '/ffmpeg-sender.php');
 // Funzioni di ABAC che si attivano per il controllo delle telecamere
 checkRecordtoStart();
 checkRecordtoEnd();
@@ -11,23 +11,44 @@ checkRecordtoEnd();
  * Verifica se ci sono record nella tabella 'prenotazioni' che hanno raggiunto il loro orario di inizio
  * e aggiorna il campo 'cam_privileges' degli allenatori corrispondenti a 1.
  * @return void
-*/
+ */
 function checkRecordtoStart()
 {
     $con = get_connection();
 
-    $sql = "SELECT COUNT(*) As count, autore_prenotazione FROM prenotazioni WHERE data_ora_inizio >= DATE_SUB(NOW(), INTERVAL 5 MINUTE)";
+    $sql = "SELECT *, COUNT(*) as count FROM prenotazioni WHERE data_ora_inizio >= DATE_SUB(NOW(), INTERVAL 5 MINUTE);";
     $query = $con->prepare($sql);
     $query->execute();
     $result = $query->fetch(PDO::FETCH_ASSOC);
 
     $count = $result['count'];
+    $squadra = $result['id_squadra'];
+    $cams = $result['cams'];
 
     if ($count > 0) {
-        $sql = "UPDATE allenatori SET cam_privileges = 1 WHERE email = :allenatore";
+
+        //Retrieve allenatori
+        $sql = "SELECT email_allenatore FROM allenatori_squadre WHERE id_squadra = :squadra";
         $query = $con->prepare($sql);
-        $query->bindParam(':allenatore', $result['autore_prenotazione']);
+        $query->bindParam(':squadra', $squadra);
         $query->execute();
+        $coaches = $query->fetchAll(PDO::FETCH_COLUMN); // Utilizziamo FETCH_COLUMN per ottenere solo la colonna 'email_allenatore'
+
+        //Retrieve ip selected cameras [TO DO]
+        $cameraRTSP = $cams; // Simulo $cams come array deglii indirizzi RTSP delle telecamere
+
+        //ffmpeg parte video-rec per ogni telecamera
+        foreach ($cameraRTSP as $rtsp) {
+            ffmpegRec($rtsp);
+        }
+
+        // Aggiorna permessi dello staff per ogni allenatore
+        foreach ($coaches as $coach) {
+            $sql = "UPDATE allenatori SET cam_privileges = 1 WHERE email = :allenatore";
+            $query = $con->prepare($sql);
+            $query->bindParam(':allenatore', $coach);
+            $query->execute();
+        }
     }
 }
 
@@ -35,7 +56,7 @@ function checkRecordtoStart()
  * Verifica se ci sono record nella tabella 'prenotazioni' che hanno raggiunto il loro orario di fine
  * e aggiorna il campo 'cam_privileges' degli allenatori corrispondenti a 0.
  * @return void
-*/
+ */
 function checkRecordtoEnd()
 {
     $con = get_connection();
--- modals/ffmpeg-sender.php
+++ modals/ffmpeg-sender.php
@@ -1,12 +1,36 @@
 <?php
 
+/**
+ * Avvia lo streaming live utilizzando FFmpeg.
+ *
+ * @param string $cameraRTSP L'URL RTSP della videocamera
+ * @return void
+ */
 function ffmpegLive($cameraRTSP) {
     $comando = 'ffmpeg -y -i'. $cameraRTSP.'-c:v libx264 -c:a aac -f dash -seg_duration 10 -window_size 6 -dash_segment_type mp4 output.mpd > /dev/null 2>&1 &';
     exec($comando);
 }
 
+/**
+ * Registra un video utilizzando FFmpeg.
+ *
+ * @param string $cameraRTSP L'URL RTSP della videocamera
+ * @return void
+ */
 function ffmpegRec($cameraRTSP) {
     $comando = 'ffmpeg -y -r 25 -rtsp_transport tcp -i' .$cameraRTSP. ' -vcodec copy -b:v 10000k out1.mp4';
     shell_exec($comando);
 }
+
+/**
+ * Interrompe la registrazione di un video in corso utilizzando FFmpeg.
+ *
+ * @param string $cameraRTSP L'URL RTSP della videocamera
+ * @return void
+ */
+function ffmpegStopRec($cameraRTSP) {
+    $comando = 'pkill -f "ffmpeg -y -r 25 -rtsp_transport tcp -i ' . $cameraRTSP . ' -vcodec copy -b:v 10000k out1.mp4"';
+    shell_exec($comando);
+}
+
 ?>
