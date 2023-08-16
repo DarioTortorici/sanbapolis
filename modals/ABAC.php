f738bffb1b007ad3f968aa16bd310fbcd3ea8760
DarioTortorici <95847496+DarioTortorici@users.noreply.github.com>
2023-08-16 16:56:01 +0200

    FFMpeg changes
    Now the code doesn't execute command in order to avoid sudo requirements

--- modals/ABAC.php
+++ modals/ABAC.php
@@ -7,16 +7,26 @@ checkRecordtoStart();
 checkRecordtoEnd();
 
 
+/**
+ * Controlla e avvia la registrazione delle prenotazioni per le telecamere.
+ *
+ * Questa funzione verifica le prenotazioni attive e avvia la registrazione dei flussi video
+ * per le telecamere associate a ciascuna prenotazione. Aggiorna anche i permessi dei
+ * membri dello staff (allenatori) per visualizzare le telecamere registrate.
+ *
+ * @return void
+ */
 function checkRecordtoStart()
 {
     $con = get_connection();
 
-    $sql = "SELECT p.*, GROUP_CONCAT(t.indirizzo_ipv4) as IPv4 COUNT(*) as count 
-        FROM prenotazioni as p 
-        INNER JOIN telecamere_prenotazioni as tp ON tp.prenotazione = p.id 
-        INNER JOIN telecamere as t on t.id = tp.telecamera
-        WHERE p.data_ora_inizio >= DATE_SUB(NOW(), INTERVAL 5 MINUTE)
-        GROUP BY p.id"; // Utilizziamo GROUP BY per ottenere il conteggio e le telecamere associate a ciascuna prenotazione
+    $sql = "SELECT p.*, GROUP_CONCAT(t.indirizzo_ipv4) as IPv4, COUNT(*) as count 
+            FROM prenotazioni as p 
+            INNER JOIN telecamere_prenotazioni as tp ON tp.prenotazione = p.id 
+            INNER JOIN telecamere as t on t.id = tp.telecamera
+            WHERE p.data_ora_inizio >= DATE_SUB(NOW(), INTERVAL 5 MINUTE)
+            GROUP BY p.id";
+
     $query = $con->prepare($sql);
     $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
@@ -24,32 +34,33 @@ function checkRecordtoStart()
     foreach ($result as $row) {
         $count = $row['count'];
         $squadra = $row['id_squadra'];
-        $cams = $row['IPv4']; // $cams conterrà un elenco di telecamere separate da virgole
+        $cams = explode(",", $row['IPv4']);
         $data = $row['data_ora_inizio'];
-    }
 
+        if ($count > 0) {
+            // Calcola la durata in secondi
+            $duration = calculateTimeDifferenceInSeconds($data, $row['data_ora_fine']);
 
-    if ($count > 0) {
-
-        //Retrieve allenatori
-        $sql = "SELECT email_allenatore FROM allenatori_squadre WHERE id_squadra = :squadra";
-        $query = $con->prepare($sql);
-        $query->bindParam(':squadra', $squadra);
-        $query->execute();
-        $coaches = $query->fetchAll(PDO::FETCH_COLUMN); // Utilizziamo FETCH_COLUMN per ottenere solo la colonna 'email_allenatore'
-
-        //ffmpeg parte video-rec per ogni telecamera
-        foreach ($cams as $Camerartsp) {
-            $rtsp =  'rtsp://istar:password@' . $Camerartsp + '/profile2/media.smp';
-            ffmpegRec($rtsp, $squadra, $data);
-        }
-
-        // Aggiorna permessi dello staff per ogni allenatore
-        foreach ($coaches as $coach) {
-            $sql = "UPDATE allenatori SET privilegi_cam = 1 WHERE email = :allenatore";
+            // Retrieve allenatori
+            $sql = "SELECT email_allenatore FROM allenatori_squadre WHERE id_squadra = :squadra";
             $query = $con->prepare($sql);
-            $query->bindParam(':allenatore', $coach);
+            $query->bindParam(':squadra', $squadra);
             $query->execute();
+            $coaches = $query->fetchAll(PDO::FETCH_COLUMN);
+
+            // FFMpeg: avvia registrazione video per ogni telecamera
+            foreach ($cams as $Camerartsp) {
+                $rtsp = 'rtsp://istar:password@' . $Camerartsp . '/profile2/media.smp';
+                $path = ffmpegRec($rtsp, $squadra, $data, $duration);
+            }
+
+            // Aggiorna permessi dello staff per ogni allenatore
+            foreach ($coaches as $coach) {
+                $sql = "UPDATE allenatori SET privilegi_cam = 1 WHERE email = :allenatore";
+                $query = $con->prepare($sql);
+                $query->bindParam(':allenatore', $coach);
+                $query->execute();
+            }
         }
     }
 }
@@ -58,9 +69,8 @@ function checkRecordtoStart()
 /**
  * Controlla e gestisce le prenotazioni scadute per chiudere le registrazioni video.
  * 
- * La funzione recupera le prenotazioni con data di fine inferiore a 5 minuti fa e
- * chiude le registrazioni video relative, aggiorna i permessi dello staff e aggiunge
- * i dettagli dei video registrati nel database.
+ * La funzione recupera le prenotazioni con data di fine inferiore a 5 minuti fa ed
+ * aggiorna i permessi dello staff e aggiunge i dettagli dei video registrati nel database.
  * 
  * @return void
  */
@@ -98,11 +108,6 @@ function checkRecordtoEnd()
         // Recupera gli indirizzi RTSP delle telecamere selezionate [TO DO]
         $cameraRTSP = $cams; // Simuliamo $cams come array di indirizzi RTSP delle telecamere
 
-        // Interrompi la registrazione video per ogni telecamera usando ffmpeg
-        foreach ($cameraRTSP as $rtsp) {
-            $path = ffmpegStopRec($rtsp, $squadra, $data);
-        }
-
         // Aggiorna i permessi dello staff
         foreach ($coaches as $coach) {
             $sql = "UPDATE allenatori SET privilegi_cam = 0 WHERE email = :allenatore";
@@ -110,9 +115,10 @@ function checkRecordtoEnd()
             $query->bindParam(':allenatore', $coach);
             $query->execute();
         }
-
+        
+        $path = "../video_editing/storage_video/". $squadra . '/' . $data . '/';
         // Aggiorna l'URL nel calendario dell'evento con la pagina per visualizzare i file
-        $watchCams = "/editing_video/video_list.php?video_location=./storage_video/".$path."&register_date=". $data;
+        $watchCams = "/editing_video/video_list.php?video_location=./storage_video/" . $path . "&register_date=" . $data;
         $sql = "UPDATE calendar_events SET url = :dir WHERE id = :idCalendar";
         $query = $con->prepare($sql);
         $query->bindParam(':dir', $watchCams);
@@ -125,4 +131,28 @@ function checkRecordtoEnd()
         $stmt->bindParam(':dir', $path);
         $stmt->execute();
     }
+}
+
+/**
+ * Calcola la differenza in secondi tra due date.
+ *
+ * Questa funzione accetta due date nel formato "YYYY-MM-DD HH:MM:SS" e calcola
+ * la differenza in secondi tra le due date, considerando giorni, ore, minuti e secondi.
+ *
+ * @param string $date1 La prima data nel formato "YYYY-MM-DD HH:MM:SS".
+ * @param string $date2 La seconda data nel formato "YYYY-MM-DD HH:MM:SS".
+ * @return int La differenza in secondi tra le due date.
+ */
+function calculateTimeDifferenceInSeconds($date1, $date2)
+{
+    $dateTime1 = new DateTime($date1);
+    $dateTime2 = new DateTime($date2);
+    $interval = $dateTime1->diff($dateTime2);
+
+    $secondsDifference = $interval->days * 24 * 60 * 60 +
+        $interval->h * 60 * 60 +
+        $interval->i * 60 +
+        $interval->s;
+
+    return $secondsDifference;
 }
--- modals/ffmpeg-sender.php
+++ modals/ffmpeg-sender.php
@@ -1,62 +1,79 @@
 <?php
 
+require 'vendor/autoload.php';
+use FFMpeg\FFMpeg;
+use FFMpeg\Format\Video\X264;
+use FFMpeg\Format\Audio\Aac;
+
 //Global variable per aiutare nel refactoring
 $cartellaStorage = "../video_editing/storage_video/";
 
 /**
- * Avvia lo streaming live utilizzando FFmpeg.
+ * Cattura un flusso RTSP da una fotocamera e lo salva come file multimediale in formato MPD.
  *
- * @param string $cameraRTSP L'URL RTSP della videocamera
+ * @param string $cameraRTSP URL del flusso RTSP della fotocamera.
  * @return void
  */
 function ffmpegLive($cameraRTSP)
 {
-    $comando = 'ffmpeg -y -i' . $cameraRTSP . '-c:v libx264 -c:a aac -f dash -seg_duration 10 -window_size 6 -dash_segment_type mp4 ../cameras/live/output.mpd > /dev/null 2>&1 &';
-    exec($comando);
+    $ffmpeg = FFMpeg::create();
+
+    $input = $ffmpeg->open($cameraRTSP);
+    
+    $format = new X264('aac', 'libx264');
+    
+    $outputFile = '../cameras/live/output.mpd';
+
+    $input->save($format, $outputFile);
 }
 
 /**
- * Avvia la registrazione di un video utilizzando FFmpeg e salva il file di output nella cartella specificata.
+ * Registra un flusso RTSP utilizzando FFMpeg.
  *
- * @param string $cameraRTSP L'URL RTSP della videocamera
- * @param string $squadra Il nome della squadra
- * @param string $data La data della registrazione
- * @return void
+ * Questa funzione cattura un flusso RTSP da una telecamera e lo registra
+ * in un file video MP4. Il file di output verrà salvato nella cartella
+ * di archiviazione specificata all'interno di una sottocartella
+ * corrispondente alla squadra e alla data.
+ *
+ * @param string $cameraRTSP URL del flusso RTSP della telecamera.
+ * @param string $squadra Nome della squadra associata alla registrazione.
+ * @param string $data Data in cui è stata effettuata la registrazione (formato: YYYY-MM-DD).
+ * @param int $durationToRecord Durata della registrazione in secondi.
+ * @return string Il percorso completo del file video MP4 registrato.
  */
-function ffmpegRec($cameraRTSP, $squadra, $data)
+function ffmpegRec($cameraRTSP, $squadra, $data, $durationToRecord)
 {
+    // Crea un'istanza di FFMpeg
+    $ffmpeg = FFMpeg::create();
+    
+    // Ottieni il percorso della cartella di archiviazione dalla variabile globale
     global $cartellaStorage;
-    $outputFolder = $cartellaStorage.$squadra.'/'.$data.'/';
 
-    // Verifica se la cartella di output esiste
+    // Costruisci il percorso completo della cartella di output
+    $outputFolder = $cartellaStorage . $squadra . '/' . $data . '/';
+
+    // Verifica e crea la cartella di output se non esiste
     if (!is_dir($outputFolder)) {
-        // Crea la cartella di output se non esiste
         mkdir($outputFolder, 0777, true);
     }
 
     // Specifica il percorso completo del file di output
     $outputPath = $outputFolder . $cameraRTSP . '.mp4';
 
-    // Comando FFmpeg con la cartella di output corretta
-    $comando = 'ffmpeg -y -r 25 -rtsp_transport tcp -i ' . $cameraRTSP . ' -vcodec copy -b:v 10000k ' . $outputPath;
-    shell_exec($comando);
-}
+    // Apri il flusso RTSP come input
+    $input = $ffmpeg->open($cameraRTSP);
+    
+    // Crea un formato di output utilizzando il codec video libx264
+    $format = new X264('aac', 'libx264');
 
-/**
- * Interrompe la registrazione di un video in corso utilizzando FFmpeg e restituisce il percorso del file di output.
- *
- * @param string $cameraRTSP L'URL RTSP della videocamera
- * @param string $squadra Il nome della squadra
- * @param string $data La data della registrazione
- * @return string Il percorso completo del file di output
- */
-function ffmpegStopRec($cameraRTSP, $squadra, $data)
-{
-    global $cartellaStorage;
-    $outputFolder = $cartellaStorage . $squadra . '/' . $data . '/';
-    $outputPath = $outputFolder . $cameraRTSP . '.mp4';
-    $comando = 'pkill -f "ffmpeg -y -r 25 -rtsp_transport tcp -i ' . $cameraRTSP . ' -vcodec copy -b:v 10000k ' . $outputPath . '"';
-    shell_exec($comando);
+    // Inizia la registrazione del flusso nel file di output specificato
+    $input->save($format, $outputPath, function () use ($durationToRecord) {
+        return [
+            '-t', $durationToRecord // Specifica la durata della registrazione
+        ];
+    });
+
+    // Restituisci il percorso del file di output
     return $outputPath;
 }
 
