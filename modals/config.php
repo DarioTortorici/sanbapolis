806cef1b03759812fc6fe0814efb9eebb00fd30e
DarioTortorici <95847496+DarioTortorici@users.noreply.github.com>
2023-08-31 10:53:39 +0200

    Revert "agg dml - modificato url di calendar events con link al video"
    This reverts commit a62353208797ef954dbcdbf1e1321f5a2aa2e749.

--- modals/config.php
+++ modals/config.php
@@ -3,9 +3,9 @@
 $domain = "https://example.com";
 
 // Dichiarazione delle variabili del database
-$databaseName = 'sanbapolis';
-$databaseUser = 'root';
-$databasePassword = '';
+$databaseName = 'your_database';
+$databaseUser = 'your_username';
+$databasePassword = 'your_password';
 $databaseHost = 'localhost';
 
 // Dichiarazione delle variabili SMTP
